2a57a9e0d82029e43f0ca8588358651d7522a0e4
Patrick Robertson <critical.wing@gmail.com>
2012-10-11 21:27:37 +0100

    Basic Spring Setup

---
+++ src/test/java/ie/cit/patrick/ExampleServiceTests.java
@@ -0,0 +1,14 @@
+package ie.cit.patrick;
+
+import ie.cit.patrick.ExampleService;
+import junit.framework.TestCase;
+
+public class ExampleServiceTests extends TestCase {
+
+	private ExampleService service = new ExampleService();
+	
+	public void testReadOnce() throws Exception {
+		assertEquals("Hello world!", service.getMessage());
+	}
+
+}
